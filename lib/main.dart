20cf7e59d30a957050a0ef959fc8b48348c023c7
mg137137 <gmihir11@gmail.com>
2023-05-23 18:26:30 +0530

    edit main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
 
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       home: LoginScreen()
     );
   }
